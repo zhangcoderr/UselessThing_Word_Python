6772b7201b7ef1719635cff3dd2fc96e024aaa95
John Smith <624662670@qq.com>
2019-06-14 16:01:31 +0800

    Update Main.py
    鸽一鸽，写的有点乱，而且写出来还么有c#实用，主要这个方法写的会误填，对输入格式要求较高

--- Main.py
+++ Main.py
@@ -17,16 +17,21 @@ def GetStringValue(pattern):
     #print(result)
     return result
 
-def FillContent(head,tail,value):#查找匹配输入
+
     #招标编号为:         的
     #head:      value   tail
-    head='招标编号为'
-    tail='的'
-    value='afsdgasdjkghasjh'
+def FillContent(head,tail,value):#查找匹配输入
+
+    # head='招标编号为'
+    # tail='的'
+    # value='afsdgasdjkghasjh'
+
     start = False
     for p in doc.paragraphs:
         if ('（封面）' in p.text):
             start = True
+        if('投标工程名称：' in p.text):
+            print(1)
         if (start):
             # if ('投标文件内容' in p.text):
             #     start = False
@@ -35,13 +40,35 @@ def FillContent(head,tail,value):#查找匹配输入
                 r = p.runs[i]
                 lastText = None#todoooooooooooooooooooooooooooooooooooooooooo
                 nextText = None
+                pLenth=len(p.runs)
                 if (i != 0):
-                    lastText = p.runs[i - 1].text
-                if (i != len(p.runs) - 1):
-                    nextText = p.runs[i + 1].text
-                if(lastText==None or nextText==None):
-                    print('nnnnnnnnnnnnnnnnnnnnnnnnnnn')
-                #print(lastText)
+                    plusMax = 2
+                    plus = 1
+                    lastText = ''
+                    while (i - plus >= 0 and plus <= plusMax):
+                        if(p.runs[i-plus].text.isspace()):
+                            plus=plus+1
+                            plusMax=plusMax+1
+                            continue
+                        else:
+                            lastText = lastText + p.runs[i - plus].text
+                            plus = plus + 1
+                    #lastText = p.runs[i - 1].text+p.runs[i].text
+                if (i != pLenth - 1):#循环取后面的字符串，遇到空格跳过,
+                    plusMax=2
+                    plus=1
+                    nextText=''
+                    while(i+plus<pLenth and plus<=plusMax):
+                        if (p.runs[i + plus].text.isspace()):
+                            plus=plus+1
+                            plusMax=plusMax+1#只匹配不是空格的字符串
+                            continue
+                        else:
+                            nextText = nextText +  p.runs[i + plus].text
+                            plus=plus+1
+
+                #if(lastText==None or nextText==None):
+                    #print('nnnnnnnnnnnnnnnnnnnnnnnnnnn')
                 headMatch=False
                 tailMatch=False
                 if(head==''):
@@ -73,7 +100,7 @@ def FillContent(head,tail,value):#查找匹配输入
 
                             r.add_text(value)
                             # r.text='afsdfa'
-                            print('ok')
+                            print(value)
 
 if __name__ == "__main__":
 
@@ -95,6 +122,8 @@ if __name__ == "__main__":
     # 误期违约金额 = GetStringValue( @ "误期违约金额.*?(?<key>.*?)元")
     # 预付款金额 = GetStringValue( @ "预付款金额(:|：).*?(?<key>.*?)预付款保函金额")
 
-    FillContent('a','b','c')
-
+    #FillContent('a','b','c')
+    FillContent('到贵方的招标编号为','的','gasjgs')
+    FillContent('的','工程','52345')
+    FillContent('投标工程名称','','fffffff')
     doc.save(r"C:\Users\Administrator\Desktop\Test\testResult.docx")
